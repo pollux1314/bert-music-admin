3f7abcdb87a2e6005d38c8250b2c26cde6bae16f
沈佳伟 <184387165@qq.com>
2022-04-16 17:36:28 +0800

    美化代码

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,26 +6,23 @@ import { sayHello } from '../api/hello'
 // })
 
 const count = ref(0)
-const message= ref('波特音乐盒')
+const message = ref('波特音乐盒')
 
-const hi=()=>{
-    // message.value="已请求"
-    sayHello().then(res=>{
-      message.value=res.data
-        console.log(res)
-    })
+const hi = () => {
+  // message.value="已请求"
+  sayHello().then(res => {
+    message.value = res.data
+    console.log(res)
+  })
 }
 </script>
 
 <template>
   <h1>{{ message }}</h1>
-    <q-btn color="primary" @click="count++">点击次数{{count}}</q-btn>
-    <br/>
-    <br/>
-    <q-btn color="primary" @click="hi">hi</q-btn>
-
+  <q-btn color="primary" @click="count++"> 点击次数{{ count }} </q-btn>
+  <br />
+  <br />
+  <q-btn color="primary" @click="hi"> hi </q-btn>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
